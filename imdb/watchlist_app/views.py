58f878d68bfab195f7f284abcd544b29866c8b60
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-08-03 18:15:07 +0530

    Update views.py

--- imdb/watchlist_app/views.py
+++ imdb/watchlist_app/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 # from .models import Movie
 # from django.http import JsonResponse
-# # Create your views here.
+
 
 # def movie_list(request):
 #     movies = Movie.objects.all()
