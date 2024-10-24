88fbc248bb6c326086b7e82dfbae14c5cc8b2fc1
Praseesh <praseeshprasee@gmail.com>
2024-10-24 09:55:19 +0530

    message

--- imdb/watchlist_app/views.py
+++ imdb/watchlist_app/views.py
@@ -1,15 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Movie
-# from django.http import JsonResponse
-
-
-# def movie_list(request):
-#     movies = Movie.objects.all()
-#     data = {
-#         'movies' : list(movies.values())
-#     }
-#     return JsonResponse(data)
 
 def movie_detail(request, pk):
     movie = Movie.objects.get(pk=pk)
